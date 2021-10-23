43db17a1a9b7db3c3856ce583999d71eea70dbe5
JonasKaruzas <jonas.karuzas@gmail.com>
2021-10-23 17:11:51 +0300

    registration hours and days

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
-
+require 'date'
 
 
 def clean_zipcode(zipcode)
@@ -42,7 +42,7 @@ end
 def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
   
-  filename = "output/thanks_#{id}.html"
+  filename = "output/thanks_#{id}.html"                                  
   
   File.open(filename, 'w') do |file|
     file.puts form_letter
@@ -50,8 +50,29 @@ def save_thank_you_letter(id, form_letter)
 
 end
 
+def number_to_weekday(num)
+  case num.wday
+  when 0
+    "Sunday"
+  when 1
+    "Monay"
+  when 2
+    "Tuesday"
+  when 3
+    "Wednesday"
+  when 4
+    "Thursday"
+  when 5
+    "Friday"
+  when 6
+    "Saturday"
+  else
+    "Something is verry bad"
+  end
+end
 
 puts 'Event Manager Initialized!'
+puts ''
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -61,19 +82,30 @@ contents = CSV.open(
   template_letter = File.read('form_letter.erb')
   erb_template = ERB.new template_letter
 
+best_hours = []
+best_day = []
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   phone = clean_phone(row[:homephone])
-
+  regdate = row[:regdate]
   # zipcode = clean_zipcode(row[:zipcode])
   # legislators = legislators_by_zipcode(zipcode)
   # form_letter = erb_template.result(binding)
   # save_thank_you_letter(id, form_letter)
-
-  puts phone
   
+  hour = Time.strptime(regdate, "%m/%d/%y %k:%M").strftime("%k")
+  best_hours.push(hour)
 
+  day = Date.strptime(regdate, "%m/%d/%y %k:%M")
+  best_day.push(number_to_weekday(day))
 
 end
+
+
+puts 'Registration hours'
+pp best_hours.tally
+puts ''
+puts 'Registration day of the week'
+pp best_day.tally
